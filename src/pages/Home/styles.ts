1cbdd61b677d9720e613069a6b92e9242f7b5ce2
guirdy <guilherme.gl1997@gmail.com>
2022-12-19 17:08:00 -0300

    feat: interrupt the cycle

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -70,7 +70,7 @@ export const CountdownContainer = styled.div`
   }
 `
 
-export const StartCountdownButton = styled.button`
+export const BaseCountdownButton = styled.button`
   width: 100%;
   border: 0;
   padding: 1rem;
@@ -78,15 +78,21 @@ export const StartCountdownButton = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
+  color: ${(props) => props.theme['gray-100']};
+
   gap: 0.5rem;
   font-weight: bold;
   cursor: pointer;
-  background: ${(props) => props.theme['green-500']};
-  color: ${(props) => props.theme['gray-100']};
+
   &:disabled {
     opacity: 0.7;
     cursor: not-allowed;
   }
+`
+
+export const StartCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme['gray-100']};
   &:not(:disabled):hover {
     background: ${(props) => props.theme['green-700']};
   }
@@ -99,4 +105,11 @@ export const Separator = styled.div`
   overflow: hidden;
   display: flex;
   justify-content: center;
+`
+
+export const StopCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['red-500']};
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['red-700']};
+  }
 `
